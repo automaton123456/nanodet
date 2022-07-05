61dff85a89a0615d33527778c42f536dc12faf95
automaton123456 <30631788+automaton123456@users.noreply.github.com>
2022-07-05 07:31:33 +0100

    Update pipeline.py

--- nanodet/data/transform/pipeline.py
+++ nanodet/data/transform/pipeline.py
@@ -14,12 +14,64 @@
 
 import functools
 import warnings
+import random
+import imgaug as ia
+import imgaug.augmenters as iaa
+from imgaug.augmentables.bbs import BoundingBox, BoundingBoxesOnImage
+
 from typing import Dict, Tuple
 
 from torch.utils.data import Dataset
 
 from .color import color_aug_and_norm
 from .warp import ShapeTransform, warp_and_resize
+
+def zoom_to_bbox(meta, bbox_index, dst_shape):
+    filename = meta['img_info']['file_name']
+    width = meta['img_info']['width']
+    height = meta['img_info']['height']
+    labels = meta['gt_labels']
+    gt_bboxes = meta['gt_bboxes']
+    all_boxes = []
+    
+    for bbox in gt_bboxes:
+        all_bboxes.append(BoundingBox(x1=bbox[0], y1=bbox[1], x2=bbox[2], y2=bbox[3]))
+        
+    if len(all_bboxes) == 0:
+        return meta
+    
+    bbs = BoundingBoxesOnImage(all_bboxes, shape=image.shape)
+    
+    zoom_bbox = bbox[bbox_index]
+    x1 = zoom_bbox[0]
+    y1 = zoom_bbox[1]
+    x2 = zoom_bbox[2]
+    y2 = zoom_bbox[3]
+
+    seq = iaa.Sequential([               
+        iaa.Affine(
+          translate_px={"x": int(-1 * (((x1+x2) / 2)- (width /2) )), "y": int(-1 * (((y1 + y2)/2)-(height / 2)))},
+        ),
+        iaa.Affine(
+            scale=(2,5)
+        ),
+        iaa.Fliplr(0.5),
+        iaa.Affine(
+          translate_percent={"x": (-0.1,0.1), "y": (-0.1,0.1)},
+        ),
+        imgaug.augmenters.size.CropToFixedSize(width=320, height=320, position="center")
+    ])
+    
+    image_aug, bbs_aug = seq(image=image, bounding_boxes=bbs)
+
+    bbs_aug = bbs_aug.clip_out_of_image()
+
+    meta["gt_bboxes"] = bbs_aug.to_xyxy_array()
+    meta["img"] = image
+    meta["height"] = 320
+    meta["width"] = 320
+   
+    return meta
 
 
 class LegacyPipeline:
@@ -54,14 +106,34 @@ class Pipeline:
         self.shape_transform = ShapeTransform(keep_ratio, **cfg)
         self.color = functools.partial(color_aug_and_norm, kwargs=cfg)
 
-    def __call__(self, dataset: Dataset, meta: Dict, dst_shape: Tuple[int, int]):
-        print('Pipeline')
-        print(meta['gt_bboxes'])
+    def __call__(self, dataset: Dataset, meta: Dict, dst_shape: Tuple[int, int]):           
+        choice = random.randint(0, 10)
+        ball_found = -1
+        club_found = -1
         
-        meta = self.shape_transform(meta, dst_shape=dst_shape)
-        
-        print('Pipeline After')
-        print(meta['gt_bboxes'])
-        
+        if 1 == 1: #choice == 1:
+            if random.randint(0, 1):
+                for i, data in enumerate(labels):
+                    if data == 1:
+                        ball_found = i;
+                        break
+                        
+                if ball_found > -1:
+                    meta = zoom_to_bbox(meta, bbox_index=ball_found, dst_shape=dst_shape)
+                else:
+                    meta = self.shape_transform(meta, dst_shape=dst_shape)
+            else:
+                for i, data in enumerate(labels):
+                    if data == 2:
+                        club_found = i;
+                        break
+                        
+                if club_found > -1:        
+                    meta = zoom_to_bbox(meta, bbox_index=club_found, dst_shape=dst_shape)
+                else:
+                    meta = self.shape_transform(meta, dst_shape=dst_shape)                 
+        else:
+            meta = self.shape_transform(meta, dst_shape=dst_shape)
+       
         meta = self.color(meta=meta)
         return meta
